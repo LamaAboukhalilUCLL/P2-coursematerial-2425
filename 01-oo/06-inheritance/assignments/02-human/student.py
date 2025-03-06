85f769a661d741614734c67dd356d7420cdcac85
Lama Abou Khalil <r0912387@ucll.be>
2025-03-06 20:21:48 +0100

    first slides

--- 01-oo/06-inheritance/assignments/02-human/student.py
+++ 01-oo/06-inheritance/assignments/02-human/student.py
@@ -1,14 +1,15 @@
 class Human:
     def __init__(self, name):
-        self.__name = name
+        self.__name = name #mangled private name if we want to access it we do obj._class__prv_var
 
     def get_name(self):
         return self.__name
 
 
-class Archer(Human):
+class Archer(Human): #inherits fromm human class
     def __init__(self, name, num_arrows):
-        pass
+        super().__init__(name) #calling super constructor  
+        self.__num_arrows= num_arrows #priv 
 
     def get_num_arrows(self):
-        return self.__num_arrows
+        return self.__num_arrows #priv 
